5295b3d97ac2179ac6fce0c6cf53b4a2839282d6
kirya <cloudkserg11@gmail.com>
2018-03-23 13:30:19 +0300

    https://chronobank.atlassian.net/projects/MD/issues/MD-219 - middleware nem - fixed tests add close and disconnect for ws and stomp clients

--- tests/index.js
+++ tests/index.js
@@ -20,7 +20,7 @@ const saveAccountForAddress = require('./helpers/saveAccountForAddress'),
   
   Stomp = require('webstomp-client');
 
-let accounts = config.dev.accounts, amqpInstance, blockHeight;
+let amqpInstance, blockHeight,  accounts = config.dev.accounts;
 
 describe('core/block processor', function () {
 
@@ -28,32 +28,34 @@ describe('core/block processor', function () {
   before(async () => {
     await saveAccountForAddress(accounts[0]);
     amqpInstance = await amqp.connect(config.rabbit.url);
-    return await clearQueues(amqpInstance);
+    await clearQueues(amqpInstance);
   });
 
-  after(() => {
-    mongoose.disconnect();
+  after(async() => {
+    await amqpInstance.close();
+    await mongoose.disconnect();
   });
 
   afterEach(async () => {
-    return await clearQueues(amqpInstance);
+    await clearQueues(amqpInstance);
   })
 
-   it('check for blockHashing -- just drop database and check that get new block', async () => {
-     await blockModel.remove();
-     await Promise.delay(5000);
-     const block = await blockModel.findOne({network: config.nis.network}).sort('-number');
-     expect(block.number).to.be.greaterThan(0);
-   });
 
-    it('check for rollback blockHashing -- just write in db wrong blocks and check t', async () => {
-      await blockModel.remove();
-      const block = {timeStamp: 1, type: 257, hash: 'sdfsdfsdf'};
-      await blockModel.findOneAndUpdate({number: 2}, block,{upsert: true});
-      await Promise.delay(5000);
-      const blockAfter = await blockModel.findOne({network: config.nis.network}).sort('-number');
-      expect(blockAfter.number).to.be.greaterThan(1);
-    });
+  it('check for blockHashing -- just drop database and check that get new block', async () => {
+    await blockModel.remove();
+    await Promise.delay(5000);
+    const block = await blockModel.findOne({network: config.nis.network}).sort('-number');
+    expect(block.number).to.be.greaterThan(0);
+  });
+
+  it('check for rollback blockHashing -- just write in db wrong blocks and check t', async () => {
+    await blockModel.remove();
+    const block = {timeStamp: 1, type: 257, hash: 'sdfsdfsdf'};
+    await blockModel.findOneAndUpdate({number: 2}, block,{upsert: true});
+    await Promise.delay(5000);
+    const blockAfter = await blockModel.findOne({network: config.nis.network}).sort('-number');
+    expect(blockAfter.number).to.be.greaterThan(1);
+  });
 
   it('send some nem from account0 to account1 and validate countMessages(2) and structure message', async () => {
 
@@ -87,7 +89,7 @@ describe('core/block processor', function () {
       (async () => {
         const channel = await amqpInstance.createChannel();  
         await connectToQueue(channel);
-        return consumeMessages(1, channel, (message) => {
+        return await consumeMessages(1, channel, (message) => {
           const content = JSON.parse(message.content);
           if (content.recipient === accounts[0]) {
               checkMessage(content);
@@ -96,22 +98,23 @@ describe('core/block processor', function () {
           return false;
         });
       })(),
-       (async () => {
-         const ws = new WebSocket('ws://localhost:15674/ws');
-         const client = Stomp.over(ws, {heartbeat: false, debug: false});
-         return await consumeStompMessages(1, client, (message) => {
-            const body = JSON.parse(message.body);
-            if (body.recipient === accounts[0]) {
-                checkMessage(body);
-                return true;
-            }
-            return false;
-         });
-       })()
+      (async () => {
+        const ws = new WebSocket('ws://localhost:15674/ws');
+        const client = Stomp.over(ws, {heartbeat: false, debug: false});
+        await consumeStompMessages(1, client, (message) => {
+          const body = JSON.parse(message.body);
+          if (body.recipient === accounts[0]) {
+              checkMessage(body);
+              return true;
+          }
+          return false;
+        });
+        return await client.disconnect();
+      })()
     ]);
   });
 
-   it('delete accounts and send transfer transaction and after delay 0 messages', async () => {
+  it('delete accounts and send transfer transaction and after delay 0 messages', async () => {
 
     await accountModel.remove();
     let tx =await createTransaction(accounts[0], 0.000001);
@@ -122,6 +125,6 @@ describe('core/block processor', function () {
     const channel = await amqpInstance.createChannel();  
     const queue =await connectToQueue(channel); 
     expect(queue.messageCount).to.equal(0);
-   });
+  });
 
 });
