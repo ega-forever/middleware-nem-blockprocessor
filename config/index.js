bb0bc9439d48f110154f7c71ba96637872900c05
kirya <cloudkserg11@gmail.com>
2018-04-22 10:57:14 +0300

    https://chronobank.atlassian.net/browse/MD-223 - nem - block processor,  fixed config/index.js for env.PROVIDERS

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ const config = {
   node: {
     network: parseInt(process.env.NETWORK) || -104,
     networkName: process.env.NETWORK_NAME || 'testnet',
-    providers: getProviders(process.env.PROVIDERS) || [{http:'http://bigalice2.nem.ninja:7890', ws: 'http://bigalice2.nem.ninja:7778'}]
+    providers: process.env.PROVIDERS && getProviders(process.env.PROVIDERS) || [{http:'http://bigalice2.nem.ninja:7890', ws: 'http://bigalice2.nem.ninja:7778'}]
   },
   consensus: {
     lastBlocksValidateAmount: parseInt(process.env.CONSENSUS_BLOCK_VALIDATE_AMOUNT) || 10
